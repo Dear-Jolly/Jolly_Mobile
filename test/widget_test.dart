aae4e4c8c1c80a5146c00bce8053d94ee2853a75
yeonjeen <jeendev2019@gmail.com>
2026-08-27 13:13:13 +0900

    test: bootstrap dependencies for app render test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:jolly_mobile/app/app.dart';
+import 'package:jolly_mobile/core/di/locator.dart';
+import 'package:jolly_mobile/core/storage/secure_storage.dart';
 
 void main() {
+  setUpAll(() {
+    FlutterSecureStorage.setMockInitialValues({});
+    if (!locator.isRegistered<SecureStorage>()) {
+      setupLocator();
+    }
+  });
+
   testWidgets('App renders', (WidgetTester tester) async {
     await tester.pumpWidget(const App());
+    await tester.pump(const Duration(seconds: 1));
     await tester.pumpAndSettle();
     expect(find.byType(App), findsOneWidget);
   });
